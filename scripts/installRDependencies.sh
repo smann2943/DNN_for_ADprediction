2c08cfcba92b8de612368a56a8021ed343e7995e
Michael Mann <Mmann2943@gmail.com>
2025-11-06 14:18:42 +0000

    made some devcontainer updates and removed the docker file

---
+++ scripts/installRDependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Rscript -e 'install.packages("openxlsx", repos="https://cloud.r-project.org")'
+Rscript -e 'install.packages("data.table", repos="https://cloud.r-project.org")'
+Rscript -e 'install.packages("parallel", repos="https://cloud.r-project.org")'
+Rscript -e 'install.packages("ggplot2", repos="https://cloud.r-project.org")'
+Rscript -e 'install.packages("pracma", repos="https://cloud.r-project.org")'
+Rscript -e 'install.packages("dgof", repos="https://cloud.r-project.org")'
+Rscript -e 'install.packages("limma", repos="https://cloud.r-project.org")'
