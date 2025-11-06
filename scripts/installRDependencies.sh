7d235bf1fc09482fca8322102735fef48ccfbb9c
Michael Mann <Mmann2943@gmail.com>
2025-11-06 19:40:07 +0000

    Updated devcontainer to install R and python in a generic container

--- scripts/installRDependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-Rscript -e 'install.packages("openxlsx", repos="https://cloud.r-project.org")'
-Rscript -e 'install.packages("data.table", repos="https://cloud.r-project.org")'
-Rscript -e 'install.packages("parallel", repos="https://cloud.r-project.org")'
-Rscript -e 'install.packages("ggplot2", repos="https://cloud.r-project.org")'
-Rscript -e 'install.packages("pracma", repos="https://cloud.r-project.org")'
-Rscript -e 'install.packages("dgof", repos="https://cloud.r-project.org")'
-Rscript -e 'install.packages("limma", repos="https://cloud.r-project.org")'
