5ae3650a45fbbd6a722a132a7957c62c0a8d724c
Michael Mann <mmann2943@gmail.com>
2025-12-12 09:57:42 +0000

    Updated the paths to files in the python script as I was seeing waring messages that files were being skipped and determined the path to the file was incorrect. Feeling this could be an issue in other parts of the code as well but will need to review ALL of the code to determine if this is the case

--- code/02 feature selection/02 Annotate_DMP.py
+++ code/02 feature selection/02 Annotate_DMP.py
@@ -36,7 +36,7 @@ def main():
 
     # Training dataset
     for i in range(1, 6):
-        input_file = "./results/k_fold_train_test/DEG/[train " + str(
+        input_file = "./results/k_fold_train_test/DMP/[train " + str(
             i) + "] AD DMP.tsv"
         if not os.path.exists(input_file):
             # Python f-strings require Python 3.6+
@@ -61,7 +61,7 @@ def main():
 
     # Training dataset
     for i in range(1, 6):
-        input_file = "./results/k_fold_train_test/DEG/[test " + str(
+        input_file = "./results/k_fold_train_test/DMP/[test " + str(
             i) + "] AD DMP.tsv"
         if not os.path.exists(input_file):
             #print(f"Warning: input file not found, skipping: {input_file}")
